f1f2eed5182070c048c3d8f62e87dd2226436f85
sutharshash <suthar.shash@gmail.com>
2022-02-22 08:00:52 +0530

    Initial Commit

---
+++ geometry.py
@@ -0,0 +1,6 @@
+import numpy
+Canvas = numpy.zeros((600, 600, 3), dtype=uint8)
+class Circle:
+    def Hollow_Circle(self):
+
+        return(Rendered_Hollow_Circle)
---
+++ main.py
@@ -0,0 +1,9 @@
+import tkinter
+import numpy
+import scipy
+import
+Window = tkinter.Tk()
+Window.state('zoomed')
+Window.title("TPS")
+
+Window.mainloop()
